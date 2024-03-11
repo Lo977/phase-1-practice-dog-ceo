827345ff3447c8c8f0d6a26f4d94975cfce160f1
Buddha Lo <lo_977@yahoo.com>
2024-03-11 11:43:29 -0700

    done

--- backup.js
+++ backup.js
@@ -1,36 +1,37 @@
-console.log("%c HI", "color: firebrick");
-
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://dog.ceo/api/breeds/image/random/2")
+  fetch("https://dog.ceo/api/breeds/image/random/4")
     .then((res) => res.json())
     .then((data) => {
       data.message.forEach((imgLink) => {
-        let img = document.createElement("img");
+        const img = document.createElement("img");
         img.src = imgLink;
         document.getElementById("dog-image-container").appendChild(img);
       });
     });
+
   fetch("https://dog.ceo/api/breeds/list/all")
     .then((res) => res.json())
     .then((data) => {
-      let breeds = data.message;
-      let breedList = document.getElementById("dog-breeds");
+      const breedList = document.getElementById("dog-breeds");
+      const breeds = data.message;
       for (let breed in breeds) {
-        let li = document.createElement("li");
+        const li = document.createElement("li");
         li.textContent = breed;
         breedList.appendChild(li);
+        console.log(breedList);
         li.addEventListener("click", () => {
           li.style.color = "red";
         });
       }
+
       document
         .getElementById("breed-dropdown")
         .addEventListener("change", (e) => {
           breedList.textContent = "";
-          for (let dogs in breeds) {
-            if (dogs[0] === e.target.value) {
-              let li = document.createElement("li");
-              li.textContent = dogs;
+          for (let breed in breeds) {
+            if (breed[0] === e.target.value) {
+              const li = document.createElement("li");
+              li.textContent = breed;
               breedList.appendChild(li);
               li.addEventListener("click", () => {
                 li.style.color = "red";
